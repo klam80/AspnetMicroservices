d1e27586ac9253449df4f9cb4d36cc9092147057
klam80 <56801780+klam80@users.noreply.github.com>
2021-04-26 10:27:45 -0400

    basket microservice developed

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Basket.API.Entities;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket); // for creating, updating in one method
+        Task DeleteBasket(string userName);
+    }
+}
